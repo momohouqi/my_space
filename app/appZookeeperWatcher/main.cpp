af433c9c04280d2a03e261f0712e14b19ad908ca
willchen <momohouqi@foxmail.com>
2018-01-06 19:49:21 +0800

    build file struct

---
+++ app/appZookeeperWatcher/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "=====app begin" << std::endl;
+    cout << "=====app end" << std::endl;
+    return 0;
+}
