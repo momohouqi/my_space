4116b13e0b7cd70d48bd4457056ef243aa4487f9
Will Chen - WCHV <will.chen@asml.com>
2018-01-10 20:42:35 +0800

    Add ZookeeperWrapper class, todo test

---
+++ lib/zookeeper/ZookeeperWrapper.h
@@ -0,0 +1,39 @@
+#ifndef ZOOKEEPER_H
+#define ZOOKEEPER_H
+#include "zookeeper.h"
+#include <string>"
+
+class ZookeeperWrapper
+{
+public:
+    enum Status {
+
+    };
+
+    ZookeeperWrapper();
+    ~ZookeeperWrapper();
+    /**
+     * @brief init
+     * @param host,ip:port
+     * @param recv_timeout
+     * @return success or not
+     */
+    bool init(const string& host, int recv_timeout);
+    /**
+     * @brief createNodeSynchronously
+     * @param path
+     * @param value
+     * @param nodeType ZOO_EPHEMERAL, ZOO_SEQUENCE,...
+     * @param returnNodePath, if nodeType is ZOO_SEQUENCE, it will be different from gived path
+     * @return
+     */
+    int createNodeSynchronously(const std::string& path, const std::string& value, int nodeType,
+                                std::string& returnNodePath);
+    int getNodeSynchronously(const std::string& path, std::string& value);
+private:
+    bool _isInitOK() const;
+private:
+    zhandle_t *m_zkHandle;
+};
+
+#endif // ZOOKEEPER_H
---
+++ lib/zookeeper/detail/ZookeeperWrapper.cpp
@@ -0,0 +1,51 @@
+#include "ZookeeperWrapper.h"
+
+ZookeeperWrapper::ZookeeperWrapper():
+    m_zkHandle(NULL)
+{
+}
+
+ZookeeperWrapper::~ZookeeperWrapper()
+{
+    if (_isInitOK()) zookeeper_close(m_zkHandle);
+}
+
+bool ZookeeperWrapper::init(const string &host, int recv_timeout)
+{
+    watcher_fn watcher_fun = NULL;
+    m_zkHandle = zookeeper_init(host.c_str(), watcher_fun, recv_timeout, 0, 0, 0);
+    if (!m_zkHandle) return false;
+}
+
+int ZookeeperWrapper::createNodeSynchronously(const std::string &path, const std::string &value, int nodeType, std::string &returnNodePath)
+{
+    if (!_isInitOK()) return ZINVALIDSTATE;
+
+    /*
+    ACL_vector acl;
+    acl.count = 1;
+    acl.data = new ACL;
+    */
+    ACL_vector ZOO_CREATOR_ALL_ACL;
+    char* returnPath[1024] = {0};
+    int status = zoo_create(m_zkHandle, path.c_str(), value.c_str(), value.length(), &acl,
+                            nodeType, returnPath, sizeof(returnPath));
+    returnNodePath = returnPath;
+    return status;
+}
+
+int ZookeeperWrapper::getNodeSynchronously(const std::string &path, std::string &value)
+{
+    if (!_isInitOK()) return ZINVALIDSTATE;
+
+    char* buff = NULL;
+    int buffLen = 0;
+    int status = zoo_get(m_zkHandle, path.c_str(), 0, buff, &buffLen, NULL);
+    value = buff;
+    return status;
+}
+
+bool ZookeeperWrapper::_isInitOK() const
+{
+    return m_zkHandle != NULL;
+}
